02e45a67ba2436faa86cb373bc2bfccfd942fb50
j-sheikh <81165556+j-sheikh@users.noreply.github.com>
2024-01-16 23:28:53 +0100

    updated files

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t rallycoding/multi-client-k8s:latest -t rallycoding/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t rallycoding/multi-server-k8s:latest -t rallycoding/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
+docker build -t rallycoding/multi-worker-k8s:latest -t rallycoding/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push rallycoding/multi-client-k8s:latest
+docker push rallycoding/multi-server-k8s:latest
+docker push rallycoding/multi-worker-k8s:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push rallycoding/multi-client-k8s:$SHA
+docker push rallycoding/multi-server-k8s:$SHA
+docker push rallycoding/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=rallycoding/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=rallycoding/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=rallycoding/multi-worker-k8s:$SHA
